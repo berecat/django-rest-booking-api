025c34905b1d68b0ed615efbcc372c60b78be81f
0xnabeix <jacklinwenp@gmail.com>
2020-12-18 21:42:55 +0300

    Create TestCurrency class for testing Currency model

--- source/trades/tests.py
+++ source/trades/tests.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -13,7 +14,101 @@ from trades import views
 
 
 class TestCurrency(APITestCase):
-    def test_list_check_status(self):
+    """Test class for Currency model"""
+
+    def setUp(self):
+        """Initialize necessary fields for testing"""
+
+        User.objects.create_user(username='test_user',
+                                 password='test')
+        self.client.login(username='test_user',
+                          password='test')
+
+    def post_currency(self, data):
+        """Post currency instance into database through web-api"""
+
         url = reverse('currency-list')
-        response = self.client.get(url)
-        assert response.status_code == 200
+        response = self.client.post(url, data, format='json')
+        return response
+
+    def test_currency_post(self):
+        """
+        Ensure we can post currency instance
+        """
+
+        data = {
+            'code': 'USD',
+            'name': 'American Dollar'
+        }
+        response = self.post_currency(data)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Currency.objects.count() == 1
+        assert Currency.objects.get().name == data['name']
+        assert Currency.objects.get().code == data['code']
+
+    def test_currencies_list(self):
+        """
+        Ensure we can retrieve the currencies collection
+        """
+
+        data_currency_1 = {
+            'code': 'USD',
+            'name': 'American Dollar',
+        }
+        self.post_currency(data_currency_1)
+
+        data_currency_2 = {
+            'code': 'EUR',
+            'name': 'Euro',
+        }
+        self.post_currency(data_currency_2)
+
+        url = reverse('currency-list')
+        response = self.client.get(url, format='json')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 2
+        assert response.data[0]['name'] == data_currency_1['name']
+        assert response.data[0]['code'] == data_currency_1['code']
+        assert response.data[1]['name'] == data_currency_2['name']
+        assert response.data[1]['code'] == data_currency_2['code']
+
+    def test_currency_get(self):
+        """
+        Ensure we can get a single currency by id
+        """
+
+        data = {
+            'code': 'USD',
+            'name': 'American Dollar'
+        }
+        response = self.post_currency(data)
+
+        url = reverse('currency-detail', None, {response.data['id']})
+        get_response = self.client.get(url, format='json')
+
+        assert get_response.status_code == status.HTTP_200_OK
+        assert get_response.data['name'] == data['name']
+        assert get_response.data['code'] == data['code']
+
+    def test_currency_update(self):
+        """
+        Ensure we can update fields for a currency
+        """
+
+        data = {
+            'code': 'BYN',
+            'name': 'American Dollar'
+        }
+        response = self.post_currency(data)
+
+        url = reverse('currency-detail', None, {response.data['id']})
+        new_data = {
+            'code': 'USD',
+        }
+        patch_response = self.client.patch(url, new_data, format='json')
+
+        assert patch_response.status_code == status.HTTP_200_OK
+        assert patch_response.data['name'] == data['name']
+        assert patch_response.data['code'] == new_data['code']
